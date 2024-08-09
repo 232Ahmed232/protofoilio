a4b8e3f11da2a4bd43864a90b52c95638bfc8f71
232Ahmed232 <x99aliahmed@gmail.com>
2024-08-09 08:11:46 +0500

    Change fronted

--- src/componenets/Frontend/Frontend.jsx
+++ src/componenets/Frontend/Frontend.jsx
@@ -20,7 +20,7 @@ function Frontend() {
                 <div className="flex-col">
                     <h1 className="font-bold text-xl my-2">Link</h1>
                     <hr className="border-white border-2" />
-                    <a href="https://65b7c4361e675b0ed67ef1d4--stunning-longma-293ead.netlify.app/" target="_blank" className="italic underline my-2 font-medium">Weber</a>
+                    <a href="https://weber-library.vercel.app/" target="_blank" className="italic underline my-2 font-medium">Weber</a>
 
                 </div>
             </div>
